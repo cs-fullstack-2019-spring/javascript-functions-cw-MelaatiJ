7a4bc5b73a6814fb741d70b5e8385ebc4e379657
Melaati Jayah <fathimajayah876@gmail.com>
2019-01-14 01:11:45 -0600

    finished

---
+++ problem3.js
@@ -0,0 +1,11 @@
+// Create a function in your program that counts from 0 to [NUMBER]
+
+function count(number)
+{
+    for (x=0; x<= number; x++)
+    {
+        console.log(x);
+    }
+}
+
+count(10);
